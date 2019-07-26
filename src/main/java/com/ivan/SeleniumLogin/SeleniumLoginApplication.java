712f42a03ca094d7c82c86bcb3d3efc400ffbc01
Ivan Zhong <ivanzhong0310@gmail.com>
2019-07-26 17:08:43 +1200

    create this project

---
+++ src/main/java/com/ivan/SeleniumLogin/SeleniumLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.SeleniumLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumLoginApplication.class, args);
+	}
+
+}
